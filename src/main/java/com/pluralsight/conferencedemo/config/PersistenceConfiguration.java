f20404bc98cd0a3a6df36264d35a18f2508e2551
Eric Kipnis <erickipnis@erics-mbp.lan>
2020-07-26 23:46:07 -0400

    Remove config

--- src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -10,13 +10,13 @@ import javax.sql.DataSource;
 public class PersistenceConfiguration {
     @Bean
     public DataSource dataSource() {
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url("jdbc:postgresql://localhost:5432/conference_app");
-        builder.username("postgres");
-        builder.password("Welcome");
-
-        System.out.println("My custom datasource bean has been set.");
-
-        return builder.build();
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.url("jdbc:postgresql://localhost:5432/conference_app");
+//        builder.username("postgres");
+//        builder.password("Welcome");
+//
+//        System.out.println("My custom datasource bean has been set.");
+//
+//        return builder.build();
     }
 }
